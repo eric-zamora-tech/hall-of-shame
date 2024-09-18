299041e54f2b7fdca8090fada4cbc0d2beea2ece
Eric Zamora <eric.zamora.in@gmail.com>
2024-09-18 12:03:32 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import ShameCard from './components/ShameCard';
 function App() {
   return (
     <div className="App">
-<ShameCard title="Example" content="Lorem ipsum" />
-<ShameCard title="Example" content="Lorem ipsum" />
-<ShameCard title="September 18, 2024" content="... my cat decided to "help" me by sitting on my keyboard whenever I tried to commit code." />
-<ShameCard title="September 18, 2024" content="...I accidentally committed all my changes to the wrong branch and was too embarrassed to fix it." />
     </div>
   );
 }
