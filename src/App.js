58322f08b28924c18f0b5577b82a650c0cc37c79
eric-zamora-tech <eric.zamora.in@gmail.com>
2024-09-18 11:52:36 -0500

    Added Shame Card

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
 <ShameCard title="Example" content="Lorem ipsum" />
 <ShameCard title="Example" content="Lorem ipsum" />
+<ShameCard title="September 18, 2024" content="... my cat decided to "help" me by sitting on my keyboard whenever I tried to commit code." />
     </div>
   );
 }
