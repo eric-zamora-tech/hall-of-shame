a4288abf5c98e1e1cdc25453c7619b71198185f2
ericzamoratech <ericzamoratech@blue-livetask8>
2024-09-23 23:35:57 +0000

    Added Shame Card

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ShameCard from './components/ShameCard';
 function App() {
   return (
     <div className="App">
+<ShameCard title="September 23, 2024" content="...I accidentally kept pushing my changes to a private repository where no one could see them." />
     </div>
   );
 }
