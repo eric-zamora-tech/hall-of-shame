53f931571c6ebc77401ea20050db6b1f8d1dfab0
ericzamoratech <ericzamoratech@blue-liveconsole2>
2024-09-24 12:04:28 +0000

    Added Shame Card

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
 <ShameCard title="September 23, 2024" content="...I accidentally kept pushing my changes to a private repository where no one could see them." />
+<ShameCard title="September 24, 2024" content="...I accidentally deleted my repository thinking it was a useless project." />
     </div>
   );
 }
