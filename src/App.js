e7c477f48c668943c661b0161e4cb7539d387b20
eric-zamora-tech <eric.zamora.in@gmail.com>
2024-09-24 14:25:21 +0000

    Added Shame Card

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
 <ShameCard title="September 23, 2024" content="...I accidentally kept pushing my changes to a private repository where no one could see them." />
 <ShameCard title="September 24, 2024" content="...I accidentally deleted my repository thinking it was a useless project." />
 <ShameCard title="September 24, 2024" content="...my cat kept sitting on my laptop keyboard and committing random code changes." />
+<ShameCard title="September 24, 2024" content="...I accidentally pushed my cat pictures to a public repository instead of my code." />
     </div>
   );
 }
