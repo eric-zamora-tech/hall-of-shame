0f315267fab686481adc1698c266b5360100d3e9
eric-zamora-tech <eric.zamora.in@gmail.com>
2024-10-01 23:36:05 +0000

    Added Shame Card

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ShameCard from './components/ShameCard';
 function App() {
   return (
     <div className="App">
+<ShameCard title="October 1, 2024" content="...I accidentally used git push --amend to push an empty commit and pretended it was intentional." />
     </div>
   );
 }
