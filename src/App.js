fb18b45dbbe6e8b4a02800dc1092c9b95a4cee9d
eric-zamora-tech <eric.zamora.in@gmail.com>
2024-10-09 23:36:08 +0000

    Added Shame Card

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
 <ShameCard title="October 6, 2024" content="...my cat discovered my laptop's power button doubles as a toy and kept turning it off whenever I tried to work." />
 <ShameCard title="October 7, 2024" content="...I accidentally deleted all my code thinking it was just a dream." />
 <ShameCard title="October 8, 2024" content="...I accidentally committed my grocery list instead of my code changes!" />
+<ShameCard title="October 9, 2024" content="...my cat kept walking on my keyboard and committing random code changes." />
     </div>
   );
 }
